0cd71a5a24a2ccb1c17e8c6e095e052000ac432a
C. George <ciogeo@gmail.com>
2015-08-24 11:15:37 +0300

    The Singleton pattern.

---
+++ design-patterns/singleton/index.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Class Database
+ */
+class Database extends PDO {
+
+    // A static variable to hold our single instance
+    private static $_instance = null;
+
+    /**
+     * Make the constructor private to ensure singleton
+     */
+    private function __construct()
+    {
+        // Call the PDO constructor
+        parent::__construct(DB_DSN, DB_USER, DB_PASSWORD);
+    }
+
+    /**
+     * A method to get our singleton instance
+     * @return Database|null
+     */
+    public static function getInstance()
+    {
+        if (!(self::$_instance instanceof Database)) {
+            self::$_instance = new Database();
+        }
+
+        return self::$_instance;
+    }
+}
+
+$db = Database::getInstance();
